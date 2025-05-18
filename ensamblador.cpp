a15ce9c4867a5a07e533684e9bc7eb97e4107632
Vanessa-qa <laura.quintero7734@alumnos.udg.mx>
2025-05-18 11:51:11 -0600

    Add files via upload

--- ensamblador.cpp
+++ ensamblador.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() 
+{
+    int cantidadNumeros, x, multiplos = 0;
+
+    cout << "Ingrese la cantidad de numeros a verificar (desde 1 hasta N): ";
+    cin >> cantidadNumeros;
+
+    cout << "Ingrese el valor de X (para contar los multiplos de X): ";
+    cin >> x;
+
+    for (int i = 1; i <= cantidadNumeros; ++i) {
+        if (i % x == 0) {
+            ++multiplos;
+        }
+    }
+
+    cout << "Hay " << multiplos << " multiplos de " << x << " entre 1 y " << cantidadNumeros << "." << endl;
+
+    return 0;
+}
